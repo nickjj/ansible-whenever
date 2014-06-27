23db0f3bfc45566891fe7d85a770634d9764629a
Nick Janetakis <nick.janetakis@gmail.com>
2014-06-27 10:39:44 -0400

    Format the time and sleep for a minute

--- tests/railsapp/config/schedule.rb
+++ tests/railsapp/config/schedule.rb
@@ -1,3 +1,6 @@
-every 1.day, at: Time.now + 3 do
+set_cron_for = Time.now + 60
+whenever_at = set_cron_for.strftime('%l:%M %P')
+
+every 1.day, at: whenever_at do
   command "echo 'whenever is awesome' > /tmp/whenever"
 end
